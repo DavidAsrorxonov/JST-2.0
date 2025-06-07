2e3a0a9df84b12b23e7d8ec36dfd5fea65a119c3
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-07 14:24:27 +0900

    feat: integrate ProfileCalendar component for event creation with date selection

--- src/components/dashboard/Events.jsx
+++ src/components/dashboard/Events.jsx
@@ -7,9 +7,22 @@ import {
   DrawerHeader,
 } from "@heroui/drawer";
 import { useDisclosure } from "@heroui/react";
+import ProfileCalendar from "./ProfileCalendar";
+import { useState } from "react";
+import { parseDate } from "@internationalized/date";
 
 const Events = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const parsedDate = new Date().toISOString().split("T")[0];
+  const [date, setDate] = useState(parseDate(parsedDate));
+  const [isDateChosen, setIsDateChosen] = useState(false);
+
+  const newFormatted =
+    date.year +
+    "-" +
+    date.month.toString().padStart(2, "0") +
+    "-" +
+    date.day.toString().padStart(2, "0");
 
   return (
     <>
@@ -30,7 +43,51 @@ const Events = () => {
         <DrawerContent>
           {(onClose) => (
             <>
-              <DrawerBody>No events yet</DrawerBody>
+              <DrawerHeader>
+                <h1 className="text-4xl font-bold">Events</h1>
+              </DrawerHeader>
+              <DrawerBody>
+                <h1 className="font-semibold text-xl">Create an event</h1>
+                <span>Choose a specific date to create an event</span>
+                <div className="flex items-center justify-center">
+                  <ProfileCalendar
+                    date={date}
+                    setDate={setDate}
+                    setDateChosen={setIsDateChosen}
+                  />
+                </div>
+
+                <div>
+                  {isDateChosen && (
+                    <>
+                      <div className="font-semibold text-center text-md">
+                        Create an event for:{" "}
+                      </div>
+                      <span className="flex items-center justify-center font-bold text-xl">
+                        {newFormatted}
+                      </span>
+                      <div className="mt-4 flex flex-col space-y-4">
+                        <label>Event name</label>
+                        <input
+                          type="text"
+                          placeholder="Event title"
+                          className="w-full p-2 border border-blue-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        />
+
+                        <label>Event description</label>
+                        <textarea
+                          className="w-full p-2 border border-blue-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                          placeholder="Event description"
+                        />
+
+                        <button className="px-4 py-1 bg-blue-100 hover:bg-blue-200 transition-all border border-blue-500 text-blue-600 rounded-md">
+                          Create
+                        </button>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </DrawerBody>
               <DrawerFooter>
                 <button
                   onClick={onClose}
--- src/components/dashboard/ProfileCalendar.jsx
+++ src/components/dashboard/ProfileCalendar.jsx
@@ -2,25 +2,15 @@ import { Calendar } from "@heroui/calendar";
 import React, { useState } from "react";
 import { parseDate } from "@internationalized/date";
 
-const ProfileCalendar = () => {
-  const parsedDate = new Date().toISOString().split("T")[0];
-  const [date, setDate] = useState(parseDate(parsedDate));
-
-  const newFormatted =
-    date.year +
-    "-" +
-    date.month.toString().padStart(2, "0") +
-    "-" +
-    date.day.toString().padStart(2, "0");
+const ProfileCalendar = ({ date, setDate, setDateChosen }) => {
+  const handleChange = (newDate) => {
+    setDate(newDate);
+    setDateChosen(true);
+  };
 
   return (
-    <div onClick={() => console.log(date)}>
-      <Calendar
-        value={date}
-        onChange={setDate}
-        onClick={() => console.log(date)}
-      />
-      <button>{newFormatted}</button>
+    <div>
+      <Calendar value={date} onChange={handleChange} />
     </div>
   );
 };
