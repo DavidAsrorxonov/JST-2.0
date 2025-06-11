15b8dafff07b70b28f51ddf208dc196a0f9cad56
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-11 16:30:41 +0900

    feat: add sorting translations and update Sorting component for localization

--- src/components/dashboard/Sorting.jsx
+++ src/components/dashboard/Sorting.jsx
@@ -2,10 +2,13 @@ import { ArrowDownAZ, ArrowDownUp, ArrowUpAZ, CircleX } from "lucide-react";
 import React, { useState } from "react";
 import { useJob } from "../../context/jobContext";
 import { useSearch } from "../../context/searchContext";
+import { useTranslation } from "react-i18next";
 
 const Sorting = () => {
   const [sortingChosen, setSortingChosen] = useState(false);
   const { setSortingType } = useSearch();
+
+  const { t } = useTranslation();
 
   return (
     <div className="flex items-center mt-5">
@@ -17,12 +20,12 @@ const Sorting = () => {
           className={`${sortingChosen ? "text-blue-600" : ""}`}
           size={20}
         />
-        Sort
+        {t("Sort")}
       </div>
 
       {sortingChosen && (
         <div className="flex gap-2">
-          <div>Sort by:</div>
+          <div>{t("Sort by")}:</div>
           <div
             className="flex gap-2 items-center justify-center bg-blue-100 px-2 rounded-full border border-blue-500 text-blue-600 cursor-pointer"
             onClick={() => setSortingType("asc")}
@@ -30,7 +33,7 @@ const Sorting = () => {
             <span>
               <ArrowUpAZ size={20} />
             </span>
-            <span>Ascending</span>
+            <span>{t("Ascending")}</span>
           </div>
           <div
             className="flex gap-2 items-center justify-center bg-blue-100 px-2 rounded-full border border-blue-500 text-blue-600 cursor-pointer"
@@ -39,7 +42,7 @@ const Sorting = () => {
             <span>
               <ArrowDownAZ size={20} />
             </span>
-            <span>Descending</span>
+            <span>{t("Descending")}</span>
           </div>
           <div
             className="flex gap-2 items-center justify-center bg-blue-100 px-2 rounded-full border border-blue-500 text-blue-600 cursor-pointer"
@@ -48,7 +51,7 @@ const Sorting = () => {
             <span>
               <CircleX size={20} />
             </span>
-            Clear sorting
+            {t("Clear sorting")}
           </div>
         </div>
       )}
