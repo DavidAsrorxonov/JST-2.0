84aaaa7ebf5a76263a6de44ae93cbcee9037c0d9
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-11 14:37:35 +0900

    feat: add ReminderContext for managing reminder events in the application

---
+++ src/context/reminderContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useState, useContext } from "react";
+
+const ReminderContext = createContext();
+
+export const ReminderProvider = ({ children }) => {
+  const [reminderEvent, setReminderEvent] = useState([]);
+
+  return (
+    <ReminderContext.Provider value={{ reminderEvent, setReminderEvent }}>
+      {children}
+    </ReminderContext.Provider>
+  );
+};
+
+export const useReminder = () => useContext(ReminderContext);
