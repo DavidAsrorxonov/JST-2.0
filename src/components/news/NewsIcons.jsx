5d2c7617dfa087723114681af7dcba425ae952b9
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-12 16:47:15 +0900

    feat: replace Newspaper icon with Globe icon in NewsIcons component

--- src/components/news/NewsIcons.jsx
+++ src/components/news/NewsIcons.jsx
@@ -1,4 +1,4 @@
-import { Newspaper } from "lucide-react";
+import { Globe, Newspaper } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -12,7 +12,7 @@ const NewsIcons = () => {
       className="w-fit h-10 rounded-xl px-4 gap-2 flex items-center justify-center bg-gray-100 hover:bg-gray-200 transition-colors cursor-pointer"
       onClick={() => navigate("/news")}
     >
-      <Newspaper size={25} className="text-gray-600" />
+      <Globe size={25} className="text-gray-600" />
       <div className="text-gray-600">{t("Latest Dev News")}</div>
     </div>
   );
