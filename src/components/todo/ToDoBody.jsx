31ac8974e4a7f65f91e3b0b6231adf44ecbb1e8b
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-17 16:34:24 +0900

    feat: restructure ToDoBody component to include ToDoBodyRight for improved layout

--- src/components/todo/ToDoBody.jsx
+++ src/components/todo/ToDoBody.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ToDoBodyLeft from "./ToDoBodyLeft";
+import ToDoBodyRight from "./ToDoBodyRight";
 
 const ToDoBody = () => {
   return (
-    <div>
-      <ToDoBodyLeft />
+    <div className="w-full flex items-center justify-center gap-2">
+      <div className="w-1/2">
+        <ToDoBodyRight />
+      </div>
+      <div className="w-1/2">
+        <ToDoBodyLeft />
+      </div>
     </div>
   );
 };
