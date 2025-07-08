4e3529efc7adc6af7365fa160eae494c8e0b0e74
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-07-08 17:21:17 +0900

    feat: enhance account deletion UI with warning message and confirmation button

--- src/components/account-delete/AccountDeleteBody.jsx
+++ src/components/account-delete/AccountDeleteBody.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Accordion, AccordionItem } from "@heroui/react";
 
 const AccountDeleteBody = () => {
+  const [showDeletingWarning, setShowDeletingWarning] = useState(true);
+
   const defaultContent =
     "Once you proceed, all of your data, including your personal information, saved settings preferences, job history, events, todos and notes will be permanently removed from our servers. This action cannot be undone, and we will not be able to help you recover any part of your account once it's deleted. Please make sure to save any important information before proceeding. If you're sure about your decision, confirm your password and proceed with the deletion. Otherwise, we recommend going back to your dashboard or contacting support if you need help.";
 
   return (
     <div>
-      <div className="flex items-center justify-center mt-10">
-        <div className="w-[50%]">
-          <Accordion variant="splitted">
-            <AccordionItem
-              startContent={<InfoIcon />}
-              key={1}
-              aria-label="Delete Account Permanently"
-              subtitle="Press to see more"
-              title="Delete Account"
-            >
-              {defaultContent}
-            </AccordionItem>
-          </Accordion>
+      {showDeletingWarning && (
+        <div className="flex items-center justify-center mt-10">
+          <div className="w-[100%] md:w-[50%]">
+            <Accordion variant="splitted">
+              <AccordionItem
+                startContent={<InfoIcon />}
+                key={1}
+                aria-label="Delete Account Permanently"
+                subtitle="Press to see more"
+                title="Delete Account"
+              >
+                {defaultContent}
+
+                <div className="flex justify-center mt-4">
+                  <button
+                    className="px-4 py-2 bg-red-500 text-white rounded-full shadow-md hover:bg-red-600 transition"
+                    onClick={() => setShowDeletingWarning(false)}
+                  >
+                    I have read the terms and I want to delete my account
+                  </button>
+                </div>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
